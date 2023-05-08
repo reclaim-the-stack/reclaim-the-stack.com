a5fb1ef48f98a06b4a90ef46026d52c4aae9d136
David Backeus <duztdruid@gmail.com>
2023-05-08 16:53:42 +0200

    Mobile hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function HeroParagraph({children}) {
 
 export function Hero() {
   return (
-    <div className="text-center w-full text-2xl text-stone-700 dark:text-stone-200">
+    <div className="text-center w-full text-xl lg:text-2xl text-stone-700 dark:text-stone-200">
       <img className="mx-auto mt-10 mb-16 w-[250px]" src="/reclaim-the-stack.png" alt="Reclaim the Stack" width="250" />
       <HeroParagraph>We spent 7 months building a Kubernetes based platform to replace Heroku for our SaaS product at <a href="https://www.mynewsdesk.com">mynewsdesk.com</a>. <strong className="dark:text-stone-100">The results were a 90% reduction in costs and a 30% improvement in performance.</strong> We also significantly improved developer experience with reduced deploy times and faster / more accessible tooling.</HeroParagraph>
 
